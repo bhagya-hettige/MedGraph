ac17da18c0277c23a38deb22649ff59e7a46a27f
Bhagya <36061726+bhagya-hettige@users.noreply.github.com>
2020-02-24 22:31:48 +1100

    Update train.py

--- train.py
+++ train.py
@@ -51,83 +51,85 @@ def train(args):
     num_iter = len(data_loader.vv_train) // args.vv_batch_size
     print('Number of iterations per epoch: {}'.format(num_iter))
 
-    with tf.Session() as sess:
-        tf.global_variables_initializer().run()
-        for epoch in range(args.num_epochs):
-            print('Training epoch: {}'.format(epoch + 1))
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            for epoch in range(args.num_epochs):
+                start_time = time.time()
+                tot_loss = 0
+                data = data_loader.sequential_randomize_vv_sequences(data_loader.vv_train_seq)
 
-            data = data_loader.randomize_vv_sequences(data_loader.vv_train_seq)
+                for iteration in range(num_iter):
+                    global_step += 1
+                    start = iteration * args.vv_batch_size
+                    end = (iteration + 1) * args.vv_batch_size if iteration < num_iter else data[0].shape[0]
 
-            for iteration in range(num_iter):
-                global_step += 1
-                start = iteration * args.vv_batch_size
-                end = (iteration + 1) * args.vv_batch_size if iteration < num_iter else data[0].shape[0]
+                    # Fetch vv sequences for the current batch
+                    (batch_vv_inputs, batch_time_train_in, batch_time_train_out, batch_out_mask,
+                     batch_vv_outputs) = data_loader.fetch_vv_batch(data, start, end)
+                    # print((batch_vv_inputs, batch_time_train_in, batch_time_train_out, batch_out_mask, batch_vv_outputs))
 
-                # Fetch vv sequences for the current batch
-                (batch_vv_inputs, batch_time_train_in, batch_time_train_out, batch_out_mask,
-                 batch_vv_outputs) = data_loader.fetch_vv_batch(data, start, end)
-                # Fetch vc edges for the current batch
-                (vc_u_i, vc_u_j, vc_label) = data_loader.fetch_vc_batch(batch_size=args.vc_batch_size, K=args.K)
+                    # Fetch vc edges for the current batch
+                    (vc_u_i, vc_u_j, vc_label) = data_loader.fetch_vc_batch(batch_size=args.vc_batch_size, K=args.K)
 
-                # Run optimization operation (backprop)
-                feed_dict_batch = {
-                    model.X_visits: sparse_feeder(data_loader.X_visits_train),
-                    model.vv_inputs: batch_vv_inputs,
-                    model.vv_outputs: batch_vv_outputs,
-                    model.vv_in_time: batch_time_train_in,
-                    model.vv_out_time: batch_time_train_out,
-                    model.vv_out_mask: batch_out_mask,
-                    model.vc_u_i: vc_u_i,
-                    model.vc_u_j: vc_u_j,
-                    model.vc_label: vc_label}
-                sess.run(model.train_op, feed_dict=feed_dict_batch)
+                    # Run optimization operation (backprop)
+                    feed_dict_batch = {
+                        model.X_visits: sparse_feeder(data_loader.X_visits_train),
+                        model.vv_inputs: batch_vv_inputs,
+                        model.vv_outputs: batch_vv_outputs,
+                        model.vv_in_time: batch_time_train_in,
+                        model.vv_out_time: batch_time_train_out,
+                        model.vv_out_mask: batch_out_mask,
+                        model.vc_u_i: vc_u_i,
+                        model.vc_u_j: vc_u_j,
+                        model.vc_label: vc_label}
+                    loss, _ = sess.run([model.loss, model.train_op], feed_dict=feed_dict_batch)
+                    tot_loss += loss
 
-                if iteration % display_freq == 0:
-                    # Calculate and display the batch loss and accuracy
-                    loss_batch = sess.run(model.loss, feed_dict=feed_dict_batch)
-                    print("Iter {0:3d}:\t Loss={1:.4f}".format(iteration, loss_batch))
+                print('----------------------------------------------------------------------------------------------------------------------------------')
+                print("Epoch {:3d}:\t Training loss: {:.4f}\t Time taken: {:.4f}sec".format(epoch + 1,
+                                                                                          tot_loss / (epoch + 1),
+                                                                                          time.time() - start_time))
 
-            # Run validation and test after every epoch
+                # Run validation and test after every epoch
 
-            # Predict for validation set
-            feed_dict_valid = {model.X_visits: sparse_feeder(data_loader.X_visits_val),
-                               model.vv_inputs: data_loader.vv_valid_seq[0],
-                               model.vv_in_time: data_loader.vv_valid_seq[1],
-                               model.vv_out_time: data_loader.vv_valid_seq[2],
-                               model.vv_out_mask: data_loader.vv_valid_seq[3]}
-            y_pred_valid = sess.run(model.y, feed_dict=feed_dict_valid)
-            # Calculate validation set evaluation metrics
-            val_auc = roc_auc_score(y_true=data_loader.vv_valid_seq[4], y_score=y_pred_valid)
-            val_ap = average_precision_score(y_true=data_loader.vv_valid_seq[4], y_score=y_pred_valid)
+                # Predict for validation set
+                feed_dict_valid = {model.X_visits: sparse_feeder(data_loader.X_visits_val),
+                                   model.vv_inputs: data_loader.vv_valid_seq[0],
+                                   model.vv_in_time: data_loader.vv_valid_seq[1],
+                                   model.vv_out_time: data_loader.vv_valid_seq[2],
+                                   model.vv_out_mask: data_loader.vv_valid_seq[3]}
+                y_pred_valid = sess.run(model.y, feed_dict=feed_dict_valid)
+                # Calculate validation set evaluation metrics
+                val_auc = roc_auc_score(y_true=data_loader.vv_valid_seq[4], y_score=y_pred_valid)
+                val_ap = average_precision_score(y_true=data_loader.vv_valid_seq[4], y_score=y_pred_valid)
 
-            # Predict for test set
-            feed_dict_test = {model.X_visits: sparse_feeder(data_loader.X_visits_test),
-                              model.vv_inputs: data_loader.vv_test_seq[0],
-                              model.vv_in_time: data_loader.vv_test_seq[1],
-                              model.vv_out_time: data_loader.vv_test_seq[2],
-                              model.vv_out_mask: data_loader.vv_test_seq[3]}
-            y_pred_test = sess.run(model.y, feed_dict=feed_dict_test)
-            # Calculate test set evaluation metrics
-            test_auc = roc_auc_score(y_true=data_loader.vv_test_seq[4], y_score=y_pred_test)
-            test_ap = average_precision_score(y_true=data_loader.vv_test_seq[4], y_score=y_pred_test)
-            test_precision = precision_score(data_loader.vv_test_seq[4][:, 1], np.argmax(y_pred_test, 1))
-            test_recall = recall_score(data_loader.vv_test_seq[4][:, 1], np.argmax(y_pred_test, 1))
+                # Predict for test set
+                feed_dict_test = {model.X_visits: sparse_feeder(data_loader.X_visits_test),
+                                  model.vv_inputs: data_loader.vv_test_seq[0],
+                                  model.vv_in_time: data_loader.vv_test_seq[1],
+                                  model.vv_out_time: data_loader.vv_test_seq[2],
+                                  model.vv_out_mask: data_loader.vv_test_seq[3]}
+                y_pred_test = sess.run(model.y, feed_dict=feed_dict_test)
+                # Calculate test set evaluation metrics
+                test_auc = roc_auc_score(y_true=data_loader.vv_test_seq[4], y_score=y_pred_test)
+                test_ap = average_precision_score(y_true=data_loader.vv_test_seq[4], y_score=y_pred_test)
+                # test_precision = precision_score(data_loader.vv_test_seq[4][:, 1], np.argmax(y_pred_test, 1))
+                # test_recall = recall_score(data_loader.vv_test_seq[4][:, 1], np.argmax(y_pred_test, 1))
 
-            # Save visit and code embeddings for test data (we use the same mapping dictionary)
-            if args.gauss:
-                mu, sigma = sess.run([model.embedding, model.sigma], feed_dict=feed_dict_test)
-                np.save('emb/%s_embedding.npy' % args.dataset,
-                        {'mu': data_loader.embedding_mapping(mu),
-                         'sigma': data_loader.embedding_mapping(sigma)})
-            else:
-                mu = sess.run(model.embedding, feed_dict=feed_dict_test)
-                np.save('emb/%s_embedding.npy' % args.dataset, data_loader.embedding_mapping(mu))
+                # Save visit and code embeddings for test data (we use the same mapping dictionary)
+                if args.gauss:
+                    mu, sigma = sess.run([model.embedding, model.sigma], feed_dict=feed_dict_test)
+                    np.save('emb/%s_embedding.npy' % args.dataset,
+                            {'mu': data_loader.embedding_mapping(mu),
+                             'sigma': data_loader.embedding_mapping(sigma)})
+                else:
+                    mu = sess.run(model.embedding, feed_dict=feed_dict_test)
+                    np.save('emb/%s_embedding.npy' % args.dataset, data_loader.embedding_mapping(mu))
 
-            print('---------------------------------------------------------')
-            print(
-                "Epoch: {0}, validation auc: {1:.01%}, validation ap: {2:.01%}, test auc: {3:.4f}, test ap: {4:.4f}, test precision: {5:.4f}, test recall: {6:.4f}".format(
-                    epoch + 1, val_auc, val_ap, test_auc, test_ap, test_precision, test_recall))
-            print('---------------------------------------------------------')
+                print(
+                    "Validation AUC: {:.4f}\t Validation AP: {:.4f}\t Test AUC: {:.4f}\t Test AP: {:.4f}\t".format(
+                        val_auc, val_ap, test_auc, test_ap))
+                print('----------------------------------------------------------------------------------------------------------------------------------')
 
 
 if __name__ == '__main__':
